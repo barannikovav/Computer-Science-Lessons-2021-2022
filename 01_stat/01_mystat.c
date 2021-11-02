9762c6f8ae721f4f9ab54381d0548dc1eada56ea
Андрей Баранников <podor@MacBook-Pro-Andrej.local>
2021-11-02 22:11:23 +0300

    Changes in directory structure

--- 01_stat/01_mystat.c
+++ 01_stat/01_mystat.c
@@ -11,17 +11,18 @@
 
 //-----------------------------------------------------------------------------------------------------------------------
 
-const char * File_type_switch (mode_t st_mode) { // switch function for type of file
-        switch (st_mode & S_IFMT) {
-           case S_IFBLK:  return "block device\n";            break;
-           case S_IFCHR:  return "character device\n";        break;
-           case S_IFDIR:  return "directory\n";               break;
-           case S_IFIFO:  return "FIFO/pipe\n";               break;
-           case S_IFLNK:  return "symlink\n";                 break;
-           case S_IFREG:  return "regular file\n";            break;
-           case S_IFSOCK: return "socket\n";                  break;
-           default:       return "unknown?\n";                break;
+const char * File_type_switch (unsigned mode) { // switch function for type of file
+        switch (mode & S_IFMT) {
+           case S_IFBLK:  return "block device";            
+           case S_IFCHR:  return "character device";        
+           case S_IFDIR:  return "directory";               
+           case S_IFIFO:  return "FIFO/pipe";               
+           case S_IFLNK:  return "symlink";                 
+           case S_IFREG:  return "regular file";            
+           case S_IFSOCK: return "socket";                                       
         }
+
+        return "unknown?";
 
 }
 
@@ -61,16 +62,16 @@ char * access_convert (uintmax_t access_code) {//function that  convert access c
 
 //-----------------------------------------------------------------------------------------------------------------------
 
-char * bad_ctime(struct timespec * time) { //function that is using instead of ctimes
-        struct tm* t_time = localtime(&(time->tv_sec));
-        long  int n_sec = time->tv_nsec;
-        char* output = (char*)calloc(40, sizeof(char));
+int my_ctime(char* out_buf, size_t buf_size, const struct timespec * file_timestamp) { //function that is using instead of ctimes
+        
+        struct tm* t_time = localtime(&(file_timestamp->tv_sec));
+        
+        char yyyymmdd_hhmmss[sizeof("YYYY-mm-dd HH:MM:SS")];
+        char tz_str[sizeof("+hhmm")];
 
-        strftime(output,  20, "%F %T", t_time);
-        sprintf(output +  19, ".%ld", n_sec);
-        strftime(output + 29, 7, " %z", t_time);
+        strftime(yyyymmdd_hhmmss, sizeof(yyyymmdd_hhmmss), "%F %T", t_time);
 
-        return output;
+        return snprintf(out_buf, buf_size, "%s.%09ld %s", yyyymmdd_hhmmss, file_timestamp->tv_nsec, tz_str);
 }
 
 //-----------------------------------------------------------------------------------------------------------------------
@@ -78,6 +79,8 @@ char * bad_ctime(struct timespec * time) { //function that is using instead of c
 int main(int argc, char *argv[])
         {
         struct stat sb;
+        char string[sizeof("YYYY-mm-dd HH:MM:SS.nnnnnnnnn +hhmm")];
+
 
            if (argc != 2) {
                fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
@@ -95,7 +98,7 @@ int main(int argc, char *argv[])
                    (uintmax_t) major(sb.st_dev),
                    (uintmax_t) minor(sb.st_dev));
 
-           printf("File type:                %s", File_type_switch(sb.st_mode));
+           printf("File type:                %s\n]", File_type_switch(sb.st_mode));
 
 
            printf("I-node number:            %ju\n", (uintmax_t) sb.st_ino);
@@ -119,27 +122,20 @@ int main(int argc, char *argv[])
                    (intmax_t) sb.st_size);
            printf("Blocks allocated:         %jd\n",
                    (intmax_t) sb.st_blocks);
+           
+           tzset();
 
-           char * s = bad_ctime(&sb.st_atimespec);
-           printf("Last status change:       %s\n", s);
-           free(s);
+           my_ctime(string, sizeof(string), &sb.st_ctimespec);
+           printf("Last status change:       %s\n", string);
 
-           s = bad_ctime(&sb.st_ctimespec);
-           printf("Last file access:         %s\n", s);
-           free(s);
+           my_ctime(string, sizeof(string), &sb.st_atimespec);
+           printf("Last file access:         %s\n", string);
 
-           s = bad_ctime(&sb.st_mtimespec);
-           printf("Last file modification:   %s\n", s);
-           free(s);
+           my_ctime(string, sizeof(string), &sb.st_mtimespec);
+           printf("Last file modification:   %s\n", string);
 
-           s = bad_ctime(&sb.st_birthtimespec);
-           printf("File creation(birth):     %s\n", s);
-           free(s);
-
-
-
-
-
+           my_ctime(string, sizeof(string), &sb.st_birthtimespec);
+           printf("File creation(birth):     %s\n", string);
 
            exit(EXIT_SUCCESS);
        }
