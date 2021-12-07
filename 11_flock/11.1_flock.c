744138f4e0fa3a6d800015dbc955e58e5f96e6b9
Andrey Barannikov <barannikov.av@phystech.edu>
2021-12-07 17:41:33 +0300

    Filework updated

--- 11_flock/11.1_flock.c
+++ 11_flock/11.1_flock.c
@@ -10,46 +10,50 @@
 int main (void) {
 	const char* file_path = "ACCESS_COUNTER.txt"; /* name of file which contains number of program launches */
 	int fd = 0;                                   /* file descriptor for opening aforementioned file */
-	int counter = 1;                              /* counter of program launches */
-
-	char buffer[sizeof("9999")] = {0};                  /* maximum amount of openings - 9999  */
+	FILE * fp;
 
 	fd = open(file_path, O_RDWR | O_CREAT, 0777); /* opening a counter-file */
 	if (fd == -1)																	/* checking for errors    */
 		handle_error("open");
 
+	fp = fdopen (fd, "w+"); /* creating file ptr form descriptor */
+	if (fp == NULL)         /* checking for errors               */
+		handle_error("fdopen");
+
+
 	if (lockf(fd, F_LOCK, 0) < 0) /* calling lockf to lock a section(fd) for exclusive use; */
-		handle_error("lockf");			 /* flag 0 means the section extends from the current file position to infinity */
+		handle_error("lockf");			/* flag 0 means the section extends from the current file position to infinity */
 
-	long int read_output = read(fd, buffer, sizeof(buffer)); /* calling read to get number from file */
-
-	if (read_output < 0) {       /* in case of error */
-		int errno_code = errno;
-		perror("read");
-		lockf(fd, F_ULOCK, 0);     /* unlocking section */
-		close(fd);                 /* closing descriptor */
+	long int number_from_file = 0;
+	if (fscanf(fp, "%ld", &number_from_file) < 1 && errno != 0) { /* getting value from counter file */
+		int errno_code = errno; /* in case of error */
+		perror("fscanf");
+		lockf(fd, F_ULOCK, 0);  /* unlocking section */
+		close(fd);              /* closing descriptor */
 		return errno_code;
 	}
 
 	sleep(3);
 
-	counter += atoi(buffer);
-	printf("The number of times the program has been launched: %d", counter);
+	++number_from_file;
+	printf("The number of times the program has been launched: %ld\n", number_from_file);
 
-	char new_value[sizeof("9999")] = {0};
-	sprintf(new_value, "%d\n", counter);
+	rewind(fp); /* setting file ptr to the beginning */
 
-	if (pwrite(fd, new_value, sizeof(new_value), 0) < 0) { /* in case of error */
-		int errno_code = errno;
-		perror("pwrite");
-		lockf(fd, F_ULOCK, 0);          /* unlocking section */
-		close(fd);                      /* closing descriptor */
+	if (fprintf(fp, "%ld\n", number_from_file) < 0) { /* printing value in counter file */
+		int errno_code = errno;  /* in case of error */
+		perror("fprintf");
+		lockf(fd, F_ULOCK, 0);   /* unlocking section */
+		close(fd);               /* closing descriptor */
 		return errno_code;
 	}
 
-	                                /* in the end of program work */
-	lockf(fd, F_ULOCK, 0);          /* unlocking section */
-	close(fd);											/* closing descriptor */
+	fflush(fp);                /* flushing output file stream */
+
+
+	/* in the end of program work */
+	lockf(fd, F_ULOCK, 0);     /* unlocking section */
+	close(fd);								 /* closing descriptor */
 
 	return 0;
 }
