75b82d4848bf8780bb9ee65c417710a0e71e34e9
Andrey Barannikov <barannikov.av@phystech.edu>
2021-12-20 06:44:34 +0300

    New program structure that should fix problems with resource leak

--- 11_flock/11.1_flock.c
+++ 11_flock/11.1_flock.c
@@ -8,52 +8,60 @@
 #include <fcntl.h>
 
 int main (void) {
-	const char* file_path = "ACCESS_COUNTER.txt"; /* name of file which contains number of program launches */
-	int fd = 0;                                   /* file descriptor for opening aforementioned file */
-	FILE * fp;
+  const char* file_path = "ACCESS_COUNTER.txt"; /* name of file which contains number of program launches */
+  int fd = 0;                                   /* file descriptor for opening aforementioned file */
+  FILE * fp;
 
-	fd = open(file_path, O_RDWR | O_CREAT, 0777); /* opening a counter-file */
-	if (fd == -1)																	/* checking for errors    */
-		handle_error("open");
+  fd = open(file_path, O_RDWR | O_CREAT, 0777); /* opening a counter-file */
+  if (fd == -1) {                               /* checking for errors    */
+    
+    perror("open");
 
-	fp = fdopen (fd, "w+"); /* creating file ptr form descriptor */
-	if (fp == NULL)         /* checking for errors               */
-		handle_error("fdopen");
+  } else {
+  	
+  	fp = fdopen (fd, "r+"); /* creating file ptr form descriptor */
 
+  	if (fp == NULL) {       /* checking for errors               */
+  		
+  		perror("fdopen");
 
-	if (lockf(fd, F_LOCK, 0) < 0) /* calling lockf to lock a section(fd) for exclusive use; */
-		handle_error("lockf");			/* flag 0 means the section extends from the current file position to infinity */
+  	} else {
+  		
+  		if (lockf(fd, F_LOCK, 0) < 0) { /* calling lockf to lock a section(fd) for exclusive use; */
+  			perror("lockf")               /* flag 0 means the section extends from the current file position to infinity */
+  		} else {
 
-	long int number_from_file = 0;
-	if (fscanf(fp, "%ld", &number_from_file) < 1 && errno != 0) { /* getting value from counter file */
-		int errno_code = errno; /* in case of error */
-		perror("fscanf");
-		lockf(fd, F_ULOCK, 0);  /* unlocking section */
-		close(fd);              /* closing descriptor */
-		return errno_code;
-	}
+  			unsigned long number_from_file = 0;
 
-	sleep(3);
+  			if (fscanf(fp, "%lu", &number_from_file) < 1 && errno != 0) { /* getting value from counter file */
+  				perror("fsacnf");
+  			} else {
 
-	++number_from_file;
-	printf("The number of times the program has been launched: %ld\n", number_from_file);
+  				sleep(3);
 
-	rewind(fp); /* setting file ptr to the beginning */
+  				++number_from_file;
+  				printf("The number of times the program has been launched: %ld\n", number_from_file);
 
-	if (fprintf(fp, "%ld\n", number_from_file) < 0) { /* printing value in counter file */
-		int errno_code = errno;  /* in case of error */
-		perror("fprintf");
-		lockf(fd, F_ULOCK, 0);   /* unlocking section */
-		close(fd);               /* closing descriptor */
-		return errno_code;
-	}
+  				rewind(fp); /* setting file ptr to the beginning */
 
-	fflush(fp);                /* flushing output file stream */
+  				if (fprintf(fp, "%ld\n", number_from_file) < 0) {
+  					perror("fprintf");
+  				} else {
 
+  					fflush(fp); /* flushing output file stream */
 
-	/* in the end of program work */
-	lockf(fd, F_ULOCK, 0);     /* unlocking section */
-	close(fd);								 /* closing descriptor */
+  				}
 
-	return 0;
+  			}
+
+  		}
+
+  	}
+  }
+
+  /* in the end of program work */
+  lockf(fd, F_ULOCK, 0);     /* unlocking section */
+  fclose(fp);                /* closing descriptor */
+
+  return 0;
 }
