11dd5cb97c35df4388ee7f20740da5e51a3035ac
Andrey Barannikov <barannikov36@gmail.com>
2025-08-19 17:18:28 +0300

    03.2_pcopy was refactored

--- 03_contentcopy/03.2_pcopy.c
+++ 03_contentcopy/03.2_pcopy.c
@@ -13,15 +13,22 @@
 
 #define BLOCK_SIZE 4096
 
-// function for convenient error handling
-#define handle_error(msg) \
-				do { int errno_code = errno; perror(msg); return errno_code; } while (0)
+//-----------------------------------------------------------------------------------------------------------------------
 
-// function for convenient error handling with freeing
-#define handle_error_free(msg, ptr) \
-				do { int errno_code = errno; perror(msg); free(ptr); return errno_code; } while (0)
+enum 
+{
+ERR_ARG = 1, // Error code 1 - wrong number of function arguments
+ERR_FILE_STAT,   // Error code 2 - failed to retrieve stat about the file
+ERR_FILE_TYPE,   // Error code 3 - unsupported file type
+ERR_FOFC,     	 // Error code 4 - failed to open file for copying
+ERR_FOFI,     	 // Error code 5 - failed to open file for inserting
+ERR_FRD,         // Error code 6 - failed to read file
+ERR_FWR,     	 // Error code 7 - failed to write file
+ERR_FIC,      	 // Error code 8 - failed to close file
+};
 
 //-----------------------------------------------------------------------------------------------------------------------
+
 ssize_t pwriteall (int fd, const void *buf, size_t count, off_t offset) // function that controls the operation of pwrite
 {
 	size_t bytes_written = 0;
@@ -31,11 +38,8 @@ ssize_t pwriteall (int fd, const void *buf, size_t count, off_t offset) // funct
 		
 		ssize_t res = pwrite(fd, buf_addr + bytes_written, count - bytes_written, offset);
 		
-		if (res < 0) {
-			
+		if (res < 0)
 			return res;
-
-		}
 		
 		bytes_written += (size_t)res;
 		offset += (int)res;
@@ -45,35 +49,27 @@ ssize_t pwriteall (int fd, const void *buf, size_t count, off_t offset) // funct
 }
 
 //-----------------------------------------------------------------------------------------------------------------------
-enum 
-{
-
-ERR_ARG_NUM = 1 
-
-};
-
-//-----------------------------------------------------------------------------------------------------------------------
 
 int main (int argc, char *argv[]) {
 
-	if (argc != 3) { // checking the number of function arguments
-		
-		fprintf(stderr, "Wrong number of function arguments");
+	if (argc != 3) { //checking the number of arguments
+			
+		fprintf(stderr, "Usage: %s SOURCE DEST\n", argv[0]);
 
-
-		return ERR_ARG_NUM;
+		return ERR_ARG;
 	}
 
 	struct stat sbet; // creating a structure to get information from lstat
 
 	if (lstat(argv[1], &sbet) == -1) { // checking lstat completion status
-       
-    handle_error("Error in lstat");
-  }
+		
+		perror("Failed to retrieve stat about the file");
+		return ERR_FILE_STAT;
+	}
 
-	if (S_ISREG(sbet.st_mode)) { // checking the file type 				
-    		
-    handle_error("Error in file type: non regular file");
+	if (!S_ISREG(sbet.st_mode)) { // checking the file type 				
+    	perror("Unsupported file type");
+		return ERR_FILE_TYPE;
 	}
 
 
@@ -82,61 +78,70 @@ int main (int argc, char *argv[]) {
 
 	if (fd_1 < 0) { // checking first file descriptor for errors
 			
-		handle_error("Failed to open first file for copying");
+		perror("Failed to open first file for copying");
+		return ERR_FOFC;
 	}
 
 	int fd_2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644); // opening or creating second file for writing only
-																																// O_TRUNC flag truncates existing file to zero length
+																  // O_TRUNC flag truncates existing file to zero length
 	if (fd_2 < 0) { // checking second file descriptor for errors
-			
-		handle_error("Failed to open second file for inserting");
+
+		perror("Failed to open second file for inserting");
+		return ERR_FOFI;
 	}
 
 
+	int ret_val = EXIT_SUCCESS;
+
 	int num_of_copied_blocks = 0;
 
-	ssize_t read_return = 1; // creating a variable to get pread completion status
-													 // default value is 1 to start while
+	ssize_t read_return = 1;    // creating a variable to get read completion status
+							    // default value is 1 to start while
 	off_t offset = 0;
 
-	while (read_return > 0) {
+	void *buf = calloc(BLOCK_SIZE, sizeof(char)); // allocating BLOCK_SIZE of memory for buffer
 
-		void *buf = calloc(BLOCK_SIZE, sizeof(char)); // allocating BLOCK_SIZE of memory for buffer
+	while (read_return > 0) {
 
 		read_return = pread(fd_1, buf, BLOCK_SIZE, offset); // getting pread completion status
 
 		if (read_return < 0) { // checking pread completion status for errors
-
-			handle_error_free("Error in file reading", buf);
+			perror("Error in file reading");
+			ret_val = ERR_FRD;
+			break;
 		}
 
 		if (pwriteall(fd_2, buf, (size_t)read_return, offset) < 0) { // checking pwriteall completion status for errors
-
-			handle_error_free("Error in file writing", buf);
+			perror("Error in file writing");
+			ret_val = ERR_FWR;
+			break;
 		} else { ++num_of_copied_blocks; }
 
 		offset += (off_t)read_return; // moving offset 
-		free(buf);
-
 	}
 
-	if (read_return == 0) { // checking final completion status 
+	free(buf);
+
+	if (read_return == 0) // checking final completion status 
 		puts("Copying completed");
-	}
+	else
+		puts("Copying failed");
 
 	printf("Number of blocks were processed: %d\n", num_of_copied_blocks);
 
 	if (close(fd_1) < 0) { // checking closing first file descriptor for errors
 
-		handle_error("Error in first file closing");
+		perror("Error in first file closing");
+		ret_val = ERR_FIC;
 
 	}
 
 	if (close(fd_2) < 0) { // checking closing second file descriptor for errors
 
-		handle_error("Error in second file closing");
+		perror("Error in second file closing");
+		ret_val = ERR_FIC;
 
 	}	
 
-	return 0;
+	return ret_val;
 }
